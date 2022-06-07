0701bb97701000b668872581fdc645385dd74385
MateoSingleton <mateokcyo@gmail.com>
2022-06-07 15:20:44 -0300

    Update Dog.js

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
+  // defino el modelo a
   sequelize.define('dog', {
     id: {
       type: DataTypes.UUID,
