562cdc7251d0683f0a8cc4dd958ed2fb53143e43
MateoSingleton <mateokcyo@gmail.com>
2022-07-08 12:47:37 -0300

    make it better

--- api/index.js
+++ api/index.js
@@ -19,10 +19,9 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-
-// Syncing all the models at once.
+const port = process.env.PORT || 3001
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(port, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
